57ad16e97511cc0ea573b7d5bf93a46aebbd297e
Dewanshi-Patel <102764724+Dewanshi-Patel@users.noreply.github.com>
2022-09-05 15:27:09 -0700

    Add files via upload

---
+++ December2021_ExlProject/src/test/java/test/AddContactTest.java
@@ -0,0 +1,66 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import page.AddContactPage;
+import page.DashboardPage;
+import page.LoginPage;
+import util.BrowserFactory;
+import util.ExcelReader;
+
+public class AddContactTest {
+	
+	WebDriver driver;
+	
+	ExcelReader exlRead = new ExcelReader("src\\main\\java\\testData\\TF_TestData.xlsx");
+	String userName = exlRead.getCellData("LoginInfo", "UserName", 2);
+	String password = exlRead.getCellData("LoginInfo", "Password", 2);
+	String dashboardHeaderVerify = exlRead.getCellData("Dashboard", "Varify Header", 2);
+	String fullName = exlRead.getCellData("AddContactInfo", "FullName", 2);
+	String companyName = exlRead.getCellData("AddContactInfo", "CompanyName", 2);
+	String email = exlRead.getCellData("AddContactInfo", "Email", 2);
+	String phone = exlRead.getCellData("AddContactInfo", "Phone", 2);
+	String address = exlRead.getCellData("AddContactInfo", "Address", 2);
+	String city = exlRead.getCellData("AddContactInfo", "City", 2);
+	String country = exlRead.getCellData("AddContactInfo", "Country", 2);
+	String state = exlRead.getCellData("AddContactInfo", "State", 2);
+	String zip = exlRead.getCellData("AddContactInfo", "Zip", 2);
+	
+	@Test
+	public void validUserShouldBeAbleToAddCustomer() throws InterruptedException {
+		
+		driver = BrowserFactory.init();
+		
+		LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+		loginPage.insertUserName(userName);
+		loginPage.insertPassword(password);
+		loginPage.clickSigninButton();
+		
+		DashboardPage dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
+		dashboardPage.validateDashboardPage(dashboardHeaderVerify);
+		dashboardPage.clickCustomerMenuButton();
+		dashboardPage.clickAddCustomerMenuButton();
+		
+		AddContactPage addContactPage = PageFactory.initElements(driver, AddContactPage.class);
+		addContactPage.validateAddContactPage("Add Contact");
+		addContactPage.insertFullName(fullName);
+		addContactPage.selectCompany(companyName);
+		addContactPage.insertEmail(email);
+		addContactPage.insertPhone(phone);
+		addContactPage.insertAddress(address);
+		addContactPage.insertCity(city);
+		addContactPage.insertState(state);
+		addContactPage.insertZip(zip);
+		addContactPage.selectCountry(country);
+		addContactPage.clickSaveButton();
+		
+		dashboardPage.clickListCustomerMenuButton();
+		
+		addContactPage.varifyInsertedNameAndDelete();
+		
+		
+	}
+
+}
